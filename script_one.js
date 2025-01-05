c2ea393049d2fd58dd0d4e79d856122d7cc6ee6c
Miroslav99 <mbochko99@gmail.com>
2025-01-05 21:34:33 +0200

    feat: add functionality for creating and deleting posts
    - Implemented `removePost` method to delete posts by index from the array.
    - Added delete button to each post card, which triggers `removePost` on click.
    - Created `displayPost` function that dynamically generates post cards and displays text.
    - Used cloning of the profile image for each post card.
    - Updated post display after deletion.
    - Styled the delete button with `.post-delete` class.

--- script_one.js
+++ script_one.js
@@ -12,19 +12,54 @@ class Post {
     addPost(information) {
         const post = new Post(information)
         this._posts.push(post)
-        
+    }
+
+    removePost(index) {
+        this._posts.splice(index, 1)
     }
 
     displayPost() {
         postsContainer.innerHTML = ''
-        this._posts.forEach(post => {
+        this._posts.forEach((post, index) => {
             const postCard = document.createElement('div')
-            postCard.innerHTML = `POST: ${post.information}`
-            postCard.classList.add('post-card')
             postsContainer.appendChild(postCard)
+            postCard.classList.add('post-card')
+
+            
+            
+            const postCardHeader = document.createElement('div')
+            postCardHeader.classList.add('post-card-header')
+            postCard.appendChild(postCardHeader)
+
+            const deleteButton = document.createElement('button')
+            deleteButton.classList.add('post-delete')
+            postCardHeader.appendChild(deleteButton)
+            deleteButton.addEventListener('click', () => {
+                this.removePost(index)
+                this.displayPost()
+            })
+
+            const profileImage = document.getElementById('profile-image').cloneNode(true)
+            postCardHeader.appendChild(profileImage)
+
+            const profileName = document.createElement('h3')
+            profileName.textContent = 'Miroslav Bochko'
+            postCardHeader.appendChild(profileName)
+
+            const onlineStatus = document.createElement('p')
+            onlineStatus.textContent = 'online'
+            postCardHeader.appendChild(onlineStatus)
+
+            const postText = document.createElement('div')
+            postText.classList.add('post-text')
+            postText.innerHTML = `${post.information}`
+            postCard.appendChild(postText)
+
+            
         })
     }
 }
+
 
 const postCreater = new Post()
 
