653783be6c3df80f098165771561c3dada2aec94
Miroslav99 <mbochko99@gmail.com>
2025-01-06 17:10:40 +0200

    The scripts were divided into two modules

--- script_one.js
+++ script_one.js
@@ -1,79 +1,20 @@
-const postsContainer = document.querySelector('.posts-container_content')
-const shareButton = document.querySelector('#share')
-const postInput = document.querySelector('#post-input')
+import { Post, postsContainer} from './script_two.js';
 
 
-class Post {
-    _posts = []
-    constructor(information) {
-        this.information = information
-    }
-
-    addPost(information) {
-        const post = new Post(information)
-        this._posts.push(post)
-    }
-
-    removePost(index) {
-        this._posts.splice(index, 1)
-    }
-
-    displayPost() {
-        postsContainer.innerHTML = ''
-        this._posts.forEach((post, index) => {
-            const postCard = document.createElement('div')
-            postsContainer.appendChild(postCard)
-            postCard.classList.add('post-card')
-
-            
-            
-            const postCardHeader = document.createElement('div')
-            postCardHeader.classList.add('post-card-header')
-            postCard.appendChild(postCardHeader)
-
-            const deleteButton = document.createElement('button')
-            deleteButton.classList.add('post-delete')
-            postCardHeader.appendChild(deleteButton)
-            deleteButton.addEventListener('click', () => {
-                this.removePost(index)
-                this.displayPost()
-            })
-
-            const profileImage = document.getElementById('profile-image').cloneNode(true)
-            postCardHeader.appendChild(profileImage)
-
-            const profileName = document.createElement('h3')
-            profileName.textContent = 'Miroslav Bochko'
-            postCardHeader.appendChild(profileName)
-
-            const onlineStatus = document.createElement('p')
-            onlineStatus.textContent = 'online'
-            postCardHeader.appendChild(onlineStatus)
-
-            const postText = document.createElement('div')
-            postText.classList.add('post-text')
-            postText.innerHTML = `${post.information}`
-            postCard.appendChild(postText)
-
-            
-        })
-    }
-}
+const shareButton = document.querySelector('#share');
+const postInput = document.querySelector('#post-input');
 
 
-const postCreater = new Post()
+const postCreater = new Post(postsContainer);
+
 
 shareButton.addEventListener('click', () => {
-    const inputValue = postInput.value 
+    const inputValue = postInput.value;
 
-    if(!inputValue) return
+    if (!inputValue) return;
 
-    postCreater.addPost(inputValue)
+    postCreater.addPost(inputValue);
+    postCreater.displayPost();
 
-    postCreater.displayPost()
-
-    postInput.value = ''
-
-    
-
-})
+    postInput.value = '';
+});
